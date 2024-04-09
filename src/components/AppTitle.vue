b819621e3bfa08202215511700c4e6adfcb396bc
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-09 15:34:58 +0200

    Feat → created AppHero Component and added to App.vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,31 @@
+<script>
+    export default{
+        data() {
+            return {
+                title: 'La mia prima App con Vue!',
+            }
+        },       
+    }
+</script>
+
+
+
+<template>
+    <div>
+        <div>
+            <h1 class="title">
+              {{ title }}
+            </h1>            
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+  .title{
+    color: crimson;
+    font-size: 50px;
+    text-align: center;
+  }
+</style>
