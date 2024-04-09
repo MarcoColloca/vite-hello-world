3d022f98c01fc33cd0b706e6edd50efabb9f890f
MarcoColloca <marco.colloca3511@gmail.com>
2024-04-09 16:00:56 +0200

    Feat → added  AppHero component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,47 @@
+<script>
+    export default{
+        data() {
+            return {
+                title: ` Titolo dell'Hero `,
+            }
+        },       
+    }
+</script>
+
+
+
+<template>
+    <div>
+        <div class="hero__section">
+            
+            <h1 class="hero-title">
+              {{ title }}
+            </h1>
+            <img class="hero__image" src="../assets/Cosmic Dream.jpg" alt="">
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+    .hero-title{
+        color: rgb(30, 14, 255);
+        font-size: 40px;
+        text-align: center;
+        margin-bottom: 75px;
+    }
+
+    .hero__section{
+        height: 100vh;
+        width: 100%;
+        padding: 100px 0;
+        background-color: rgb(0, 0, 0);
+        position: relative;
+    }
+
+    .hero__image{
+        max-width: 100%;
+        aspect-ratio: 1/1;
+    }
+</style>
